3105c11b701bb476799e182bb95e63d5f42ad720
Min Shin Khant <minshinkhant7@gmail.com>
2020-11-16 14:19:18 -0500

    Update settings.py

--- planetier/settings.py
+++ planetier/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(zb2$245e^kc9j8_&dwokspctm38si7ni-eg7ly$^np*am%wm$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
